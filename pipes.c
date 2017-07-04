26cee4ef68bb105ab5aefe35c3c402393bde32bf
Jon Bakies <jon@baki.es>
2017-07-03 20:27:42 -0400

    Commented better

--- pipes.c
+++ pipes.c
@@ -36,21 +36,28 @@ int main(void) {
         return 1;
     }
     if (pid > 0) { /* parent process */
-        /* write to the pipe */
+
+        /* write to the pipe and then close cause we done */
         write(fd[WRITE_END], write_msg, strlen(write_msg)+1);
         printf("P wrote %s\n",write_msg);
-        /* close the write end of the pipe */
         close(fd[WRITE_END]);
+	
+	// wait for a message to come in the pipe and print it
 	read(fd[READ_END], write_msg, strlen(read_msg) + 1);
 	printf("P read %s\n", write_msg);
+
     }
     else { /* child process */
-        /* read from the pipe */
+
+        /* read from the pipe and close it cause we done */
         read(fd[READ_END], read_msg, BUFFER_SIZE); 
 	printf("C read %s\n",read_msg);
-        /* close the write end of the pipe */
         close(fd[READ_END]);
+	
+	// Switch the case
 	switchCase(read_msg);
+
+	// Write the message back to the parent 
 	write(fd[WRITE_END], read_msg, strlen(read_msg) + 1);
 	printf("C wrote %s\n", read_msg);
         close(fd[WRITE_END]);
